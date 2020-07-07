f289c13e1704ebac18358f020ac5cb2c246a872a
DavidQF555 <davidfeng2003@gmail.com>
2020-07-07 06:14:00 -0700

    Showed Character Base Stats

--- src/main/java/com/david/gachabot/commands/CharacterListCommand.java
+++ src/main/java/com/david/gachabot/commands/CharacterListCommand.java
@@ -9,15 +9,63 @@ import net.dv8tion.jda.api.entities.Message;
 
 public class CharacterListCommand extends Command {
 
+	private static int NAME_LENGTH = 25;
+	private static int RATE_LENGTH = 15;
+	private static int HP_LENGTH = 7;
+	private static int ATTACK_LENGTH = 7;
+	private static int DEFENSE_LENGTH = 7;
+	
 	@Override
 	public void onCommand(Message m) {
 		String out = m.getAuthor().getAsMention() + " The following are all the characters: ```";
+		String descName = "Name";
+		while(descName.length() < NAME_LENGTH) {
+			descName += " ";
+		}
+		String descRate = "Rate";
+		while(descRate.length() < RATE_LENGTH) {
+			descRate += " ";
+		}
+		String descHP = "HP";
+		while(descHP.length() < HP_LENGTH) {
+			descHP += " ";
+		}
+		String descAtt = "Attack";
+		while(descAtt.length() < ATTACK_LENGTH) {
+			descAtt += " ";
+		}
+		String descDef = "Defense";
+		while(descDef.length() < DEFENSE_LENGTH) {
+			descDef += " ";
+		}
+		out += "\n" + descName.substring(0, NAME_LENGTH) + " " + descRate.substring(0, RATE_LENGTH) + " " + descHP.substring(0, HP_LENGTH) + " " + descAtt.substring(0, ATTACK_LENGTH) + " " + descDef.substring(0, DEFENSE_LENGTH);
+		String div = "";
+		while(div.length() < NAME_LENGTH + RATE_LENGTH + HP_LENGTH + ATTACK_LENGTH + DEFENSE_LENGTH + 4) {
+			div += "-";
+		}
+		out += "\n" + div;
 		for(LocalCharacterData data : Bot.characters.values()) {
 			String name = data.getName();
-			while(name.length() < 25) {
+			while(name.length() < NAME_LENGTH) {
 				name += " ";
 			}
-			out += "\n" + name.substring(0, 25) + " " + getRoundedPercent(data.getRate(), 2);
+			String rate = getRoundedPercent(data.getRate(), 2);
+			while(rate.length() < RATE_LENGTH) {
+				rate += " ";
+			}
+			String hp = "" + data.getBaseHP();
+			while(hp.length() < HP_LENGTH) {
+				hp += " ";
+			}
+			String attack = "" + data.getBaseAttack();
+			while(attack.length() < ATTACK_LENGTH) {
+				attack += " ";
+			}
+			String defense = "" + data.getBaseDefense();
+			while(defense.length() < DEFENSE_LENGTH) {
+				defense += " ";
+			}
+			out += "\n" + name.substring(0, NAME_LENGTH) + " " + rate.substring(0, RATE_LENGTH) + " " + hp.substring(0, HP_LENGTH) + " " + attack.substring(0, ATTACK_LENGTH) + " " + defense.substring(0, DEFENSE_LENGTH);
 		}
 		m.getChannel().sendMessage(out + "```").queue();
 	}
