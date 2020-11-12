237eb0a1439bfa3856cf1aaf63f587fe247523ef
DavidQF555 <davidfeng2003@gmail.com>
2020-11-12 02:06:14 -0800

    Added Command Tag to Settings

--- src/main/java/com/davidqf/gachabot/Reference.java
+++ src/main/java/com/davidqf/gachabot/Reference.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Reference {
 
     public final static String TOKEN = getStringSetting("token");
-    public final static String COMMAND = "?";
+    public final static String COMMAND = getStringSetting("command");
     public final static long OWNER_ID = getLongSetting("owner_id");
     public final static String ATTACK_CODEPOINTS = "U+1f3f9";
     public final static List<String> SWAP_CODEPOINTS = new ArrayList<>(Arrays.asList("U+31U+20e3", "U+32U+20e3", "U+33U+20e3", "U+34U+20e3", "U+35U+20e3"));
